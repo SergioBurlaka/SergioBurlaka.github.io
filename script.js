52eeaee6300a84057fbfc8a833555c0a8f7ab204
SergioBurlaka <burlaka01@gmail.com>
2017-08-22 22:51:49 +0300

    first comit CV files
    CV first commit and push of CV files

---
+++ script.js
@@ -0,0 +1,76 @@
+
+$(document).ready(function () {
+//     $(".boxButton").click(function () {
+//         $(".inputWindow").slideToggle("slow");
+//     })
+// });
+//
+// var arrDicton = [];
+//
+// function saveTransltn() {
+//     var onEnglish = $('#englishWord').val();
+//
+//     var onRussian = $('#russianWord').val();
+//
+//
+//     if(onEnglish == ""){
+//         alert("Enter the word on English")
+//     }
+//     if(onRussian == ""){
+//         alert("Enter the word on Russian")
+//     }
+//
+//     // alert(onEnglish);
+//
+//
+//     if(onEnglish !== "" && onRussian !== "" ){
+//
+//         var wordObj = new Word(onEnglish, onRussian);
+//
+//
+//         arrDicton.push(wordObj);
+//
+//         document.getElementById("englishWord").value = "";
+//         document.getElementById("russianWord").value = "";
+//
+//         // console.log(arrDicton);
+//     }
+//
+//
+// }
+//
+//
+// function Word (onEnglish, onRussian) {
+//     var vm = this;
+//     vm.onEnglish = onEnglish;
+//     vm.onRussian = onRussian;
+//     // vm.rating = 0;
+//     vm.fail = 0;
+//     vm.success = 0;
+//     vm.successLenght =0;
+//     vm.incrementFail = function () {
+//         vm.fail++;
+//         vm.successLenght = 0;
+//     };
+//
+//     vm.incrementSuccess = function () {
+//         vm.success++;
+//         vm.successLenght++;
+//     }
+//
+// }
+//
+//
+//
+//
+//
+// function printAllWords() {
+//
+//     for (var i = 0; i < arrDicton.length; i++) {
+//         var obj = arrDicton[i];
+//         console.log(obj);
+//     }
+
+});
+
+
